e35485748fa0262af9526b8d596bac1e4735ece8
Jan <Jan@Jan-PC>
2012-03-05 14:21:46 +0100

    SneeuwMan class gemaakt.

---
+++ Opdracht 1/SneeuwMan.cpp
@@ -0,0 +1,31 @@
+/*
+ * SneeuwMan.cpp
+ *
+ *  Created on: 5 mrt. 2012
+ *      Author: Jan
+ */
+
+#include "SneeuwMan.hpp"
+
+
+//constructor
+SneeuwMan::SneeuwMan(int positionX, int positionY) {
+	this->positionX = positionX;
+	this->positionY = positionY;
+}
+
+
+void SneeuwMan::move(int direction) {
+
+}
+
+
+//wordt elke "tick" aangeroepen om het winterlandschap, sneeuwman en sneeuwvlokken te tekenen
+void SneeuwMan::draw() {
+
+}
+
+
+
+
+
