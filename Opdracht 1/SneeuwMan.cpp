f46e4a096e665de67c2d2f3ea3b3e70a6441030a
Jan <Jan@Jan-PC>
2012-03-05 15:25:46 +0100

    commenting sneeuwman class

--- Opdracht 1/SneeuwMan.cpp
+++ Opdracht 1/SneeuwMan.cpp
@@ -8,20 +8,27 @@
 #include "SneeuwMan.hpp"
 
 
-//constructor
+//constructor, initiele x en y positie setten.
 SneeuwMan::SneeuwMan(int positionX, int positionY) {
 	this->positionX = positionX;
 	this->positionY = positionY;
 }
 
 
+//Beweeg function van de sneeuwman. X positie + wijziging van de xpositie is de nieuw positie
 void SneeuwMan::move(int direction) {
 	this->positionX = this->positionX+direction;
 }
 
+//Teken functie van de sneeuwman. Word aangeroepen vanaf sneeuwlandschap class
 void SneeuwMan::draw() {
+
+	//kies kleur
 	maSetColor( 0xebdbdbd );
+
+	//teken sneeuwman romp
 	maFillRect( this->positionX, this->positionY-30, 20, 30 );
+	//teken sneeuwman hoofd
 	maFillRect( this->positionX+4, this->positionY-42, 12, 12 );
 }
 
